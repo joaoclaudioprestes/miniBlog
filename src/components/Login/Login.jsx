f7a75c2cc13f8c84f187fd6613119cc727f9d72e
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-06-07 22:37:15 -0300

    feat: updated login and register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,19 +9,19 @@ function Login() {
       <Header />
       <Container>
         <section className={style.section}>
-          <h1>Login</h1>
-          <p>Acesse sua conta para compartilhar suas histórias</p>
+          <h1 className={style.title}>Login</h1>
+          <p className={style.description}>Acesse sua conta para compartilhar suas histórias</p>
           <form action="" className={style.form}>
-            <label htmlFor="email">
+            <label htmlFor="email" className={style.label}>
               <span>E-mail:</span>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" required className={style.input} />
             </label>
-            <label htmlFor="password">
+            <label htmlFor="password" className={style.label}>
               <span>Senha:</span>
-              <input type="password" id="password" name="password" required />
+              <input type="password" id="password" name="password" required className={style.input} />
             </label>
-            <button type="submit">Login</button>
-            <span>
+            <button type="submit" className={style.button}>Login</button>
+            <span className={style.register}>
               Não possui cadastro? <a href="/register">Ir para cadastro</a>
             </span>
             <span className={style.message}></span>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -9,24 +9,24 @@ function Register() {
       <Header />
       <Container>
         <section className={style.section}>
-          <h1>Cadastre-se para acessar o Blog</h1>
-          <p>Entre para compartilhar suas histórias</p>
+          <h1 className={style.title}>Cadastre-se para acessar o Blog</h1>
+          <p className={style.subtitle}>Entre para compartilhar suas histórias</p>
           <form action="" className={style.form}>
             <label htmlFor="nome">
-              <span>Nome:</span>
-              <input type="text" id="nome" name="nome" required />
+              <span className={style.label}>Nome:</span>
+              <input type="text" id="nome" name="nome" required className={style.input} />
             </label>
             <label htmlFor="email">
-              <span>E-mail:</span>
-              <input type="email" id="email" name="email" required />
+              <span className={style.label}>E-mail:</span>
+              <input type="email" id="email" name="email" required className={style.input} />
             </label>
             <label htmlFor="password">
-              <span>Senha:</span>
-              <input type="password" id="password" name="password" required />
+              <span className={style.label}>Senha:</span>
+              <input type="password" id="password" name="password" required className={style.input} />
             </label>
-            <button type="submit">Cadastrar</button>
+            <button type="submit" className={style.button}>Cadastrar</button>
             <span>
-              Já possui cadastro? <a href="/login">Ir para Login</a>
+              Já possui cadastro? <a href="/login" className={style.link}>Ir para Login</a>
             </span>
             <span className={style.message}></span>
           </form>
